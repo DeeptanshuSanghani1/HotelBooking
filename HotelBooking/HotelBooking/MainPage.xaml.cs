7329e978aeb644a791a326c2d51f0e7c23c0cd0a
DeeptanshuSanghani1 <99564490+DeeptanshuSanghani@users.noreply.github.com>
2022-08-08 14:27:44 -0400

    Created Initial Project and Main Form Page

---
+++ HotelBooking/HotelBooking/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HotelBooking
+{
+    public partial class MainPage : ContentPage
+    {
+        //Create a List of all Rooms
+        List<Room> _roomsList = new List<Room>();
+        Room _room;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CreateRooms();        //Method to add rooms to the List and update the picker with the available rooms
+        }
+
+        public void CreateRooms()
+        {
+            _roomsList.Clear();
+
+            _roomsList.Add(new Room
+            {
+                RoomId = 1,
+                RoomType = "Single Room",
+                Roomrate = 65,
+                Vacant = 25
+            });
+
+            _roomsList.Add(new Room
+            {
+                RoomId = 2,
+                RoomType = "Double Room",
+                Roomrate = 95,
+                Vacant = 25
+            });
+
+            _roomsList.Add(new Room
+            {
+                RoomId = 3,
+                RoomType = "Twin Room",
+                Roomrate = 85,
+                Vacant = 10
+            });
+
+            _roomsList.Add(new Room
+            {
+                RoomId = 4,
+                RoomType = "Triple Room",
+                Roomrate = 125,
+                Vacant = 10
+            });
+            _roomsList.Add(new Room
+            {
+                RoomId = 5,
+                RoomType = "Quad Room",
+                Roomrate = 185,
+                Vacant = 5
+            });
+            _roomsList.Add(new Room
+            {
+                RoomId = 6,
+                RoomType = "Delux Room",
+                Roomrate = 200,
+                Vacant = 5
+            });
+            roomSelect.ItemsSource = _roomsList;
+        }
+
+        private async void GoToBookingPage(object sender, EventArgs e)
+        {
+            _room = roomSelect.SelectedItem as Room;
+            //await Navigation.PushAsync(new BookingForm(_room));
+        }
+    }
+}
---
+++ HotelBooking/HotelBooking/Room.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace HotelBooking
+{
+    public class Room
+    {
+        //Creating variables for room properties
+        private int _roomId;
+        private string _roomType;
+        private int _roomrate;
+        private int _roomVacant;
+
+        public int RoomId
+        {
+            get { return _roomId; }
+            set { _roomId = value; }
+        }
+
+        public string RoomType
+        {
+            get { return _roomType; }
+            set { _roomType = value; }
+        }
+
+        public int Roomrate
+        {
+            get { return _roomrate; }
+            set { _roomrate = value; }
+        }
+
+        public int Vacant
+        {
+            get { return _roomVacant; }
+            set { _roomVacant = value; }
+        }
+
+        public Room() { }
+
+        public Room(int roomID, string roomType, int roomRate, int roomVacant)
+        {
+            this._roomId = roomID;
+            this._roomType = roomType;
+            this._roomrate = roomRate;
+            this._roomVacant = roomVacant;
+        }
+    }
+}
